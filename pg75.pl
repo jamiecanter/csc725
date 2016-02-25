31f0d6db93e71949ad5ad37efbfe923b7bfe34ba
jamiecanter <3364309@myuwc.ac.za>
2016-02-25 09:06:28 +0200

    Update pg75.pl

--- pg75.pl
+++ pg75.pl
@@ -5,20 +5,25 @@ sub main
 {
   my $directory = '/home/jamie/Documents/PerlIP/Assignment 2/Dummy/sub1';
   opendir( my $DIR, $directory );
-    
-    
-    opendir(DIR, $directory);
-    my @folders;
+  my @folders;
+  push @folders, $directory;
+  closedir($DIR);
+    while ( my $pwd = shift @folders ) {
+      print $pwd."\n";
+
+    opendir(DIR, $pwd);
 
     while ( my $entry = readdir DIR ) {
-    if($entry =~ m/\.txt$/){
+    if($entry =~ m/\.jpg$/){
       print($entry."\n");
     }
     next unless -d $directory . '/' . $entry;
     next if $entry eq '.' or $entry eq '..';
-    print($directory."/".$entry);
-    print("\n");
- 
+    print $entry."\n";
+    push @folders,$directory."/".$entry;
+
+}
+
 }
 
 
